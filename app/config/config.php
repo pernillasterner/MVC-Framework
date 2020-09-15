6841073f3297861aa3c7a6495f082eb5b0da9bb7
Pernilla Sterner <psterner@hotmail.se>
2020-09-15 14:00:08 +0200

    Define CONSTANTS for paths/url and Add autoloader function for libraries

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/mvc-framework');
+  // Site Name
+  define('SITENAME', 'MVC_Framework');
