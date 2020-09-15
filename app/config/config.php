72f9799781e10bbf51ec1b3a129eab8d5ade1336
Pernilla Sterner <psterner@hotmail.se>
2020-09-15 15:17:50 +0200

    Create connection to db using PDO

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+  // DB Params
+  define('DB_HOST', 'HOST');
+  define('DB_USER', 'USER');
+  define('DB_PASS', 'PASSWORD');
+  define('DB_NAME', 'DB NAME');
+
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
