b7c0afc333fe29c5983a34da71c3b3b467930aaf
Pernilla Sterner <psterner@hotmail.se>
2020-09-15 11:12:51 +0200

    Loading the controller from the URL

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -12,12 +12,34 @@ class Core
 
   public function __construct() 
   {
-      $this->getUrl();
+      $url = $this->getUrl();
+
+      // Look into controller for controller (first value)
+      if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+        // If exists, set as current controller
+        $this->currentController = ucwords($url[0]);
+        // Unset 0 Index
+        unset($url[0]);
+      }
+
+      // Require the controller
+      require_once '../app/controllers/' . $this->currentController . '.php';
+
+      // Instaniate the controller class
+      $this->currentController = new $this->currentController;
   }
 
   // Get the current url
   public function getUrl() 
   {
-      echo $_GET['url'];
+      if(isset($_GET['url'])) {
+        // Remove ending slash
+        $url = rtrim($_GET['url'], '/');
+        // Sanitize
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        // Break it into an array
+        $url = explode('/', $url);
+        return $url;
+      } 
   }
 }
